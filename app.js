42b6eece2883398fb1a85c62c7faff240165de76
ewang193 <elina.wang@pwc.com>
2017-08-28 11:42:18 +0800

    use home.html to render page

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var express = require('express');
+var exphbs = require('express-handlebars');
+
 var app = express();
 
+app.engine('.html', exphbs({
+    defaultLayout: 'main'
+    // extname: '.html'
+}));
+app.set('view engine', '.html');
+
 app.get('/', function(req, res){
-    res.send('Hello World!');
+    res.render('home');    //用home.html中的内容来渲染页面
+    // res.send('home');   //直接显示send后面括号里的内容
 });
 
 app.listen(5020, function(){
